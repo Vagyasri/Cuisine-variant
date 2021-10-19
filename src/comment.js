43debba023cfbdcfa128a1c67eb1bd98f96481df
Lucky <vagyasri2011@gmail.com>
2021-10-19 17:50:37 +0200

    Align popup

--- src/comment.js
+++ src/comment.js
@@ -12,7 +12,9 @@ const display = () => {
     <input type="text" class="userName" id="name-id" name="user-name" placeholder="Type Your Name" required>
     <textarea class="userComment" id="comment-id" name="comment-name" placeholder="Comment here ..." rows="10" cols="50" required></textarea>
     <button type="submit" class="comm-sub">Submit</button>
-    </form>`;
+    </form>
+    <ul class="comment-list"> </ul>
+    <p>comment counts</p>`;
 };
 
 export default display();
