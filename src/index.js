34b91f3e2c6f8f8c304dfa6b875483a57eaaff30
Lucky <vagyasri2011@gmail.com>
2021-10-19 12:12:05 +0200

    Access Image from JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import Icon from './logo-small.jpeg';
+
+function component() {
+  const element = document.createElement('div');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
