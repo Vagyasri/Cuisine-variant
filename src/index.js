3fd620626007b9e60c83662b4ce714e668abf300
Lucky <vagyasri2011@gmail.com>
2021-10-19 13:15:57 +0200

    Add Logo using URL

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
 import './style.css';
-import Icon from './Img/logo-small.jpeg';
-
-const element = document.querySelector('.logo');
-function component() {
-  const image = document.createElement('div');
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  image.appendChild(myIcon);
-
-  return image;
-}
-
-element.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -22,18 +22,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
     ],
   },
 };
