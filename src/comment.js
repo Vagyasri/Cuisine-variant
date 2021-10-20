af47dd16e43cf380c3d6f77a1d839d6d5233a463
Vagyasri <vagyasri2011@gmail.com>
2021-10-20 18:17:18 +0200

    Link image to button

--- src/comment.js
+++ src/comment.js
@@ -4,19 +4,19 @@ import { getMeals } from './displayImage.js';
 const commentSection = document.querySelector('.comment-popup');
 
 const displayPop = async () => {
-  console.log('getMeals');
   getMeals().then((getMeal) => {
+    console.log(getMeal);
     commentSection.innerHTML = `<div class="popup-top">
     <div class="meal-data">
-    <h3> ${getMeal.meals[0].strMeal} <h3>
-    <img src="${getMeal.meals[0].strMealThumb}" alt="Meal Image" class="meal-img">
+    <h3> ${getMeal[0].strMeal} <h3>
+    <img src="${getMeal[0].strMealThumb}" alt="Meal Image" class="meal-img">
     </div>
 
     <div class="com-count">
     <img src="https://c.tenor.com/CePpbsVdFRMAAAAC/love-beating.gif" alt="heart-icon" class="heart-icon"> <span class="like-count">likes count</span>
     </div>
 
-    <p class="instructions"> <b>Instructions:</b><br><br>${getMeal.meals[0].strInstructions} </p>
+    <p class="instructions"> <b>Instructions:</b><br><br>${getMeal[0].strInstructions} </p>
 </div>
 
 <form class="popup-bottom">
