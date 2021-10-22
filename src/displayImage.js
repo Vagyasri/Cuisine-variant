706324afc0eac22a4dfa03bf3957a87062b4660b
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-10-22 12:46:17 +0100

    Add all items counter for home page

--- src/displayImage.js
+++ src/displayImage.js
@@ -1,31 +1,58 @@
 // eslint-disable-next-line import/no-cycle
 import accessComment from './accessButton.js';
+import { allHearts, likesDisplay } from './likes_api.js';
 
 const foodApi = 'https://themealdb.com/api/json/v1/1/categories.php';
 const mealList = document.querySelector('.meal-list');
+const itemList = document.querySelector('.item-list');
 
 const getMeals = async () => {
   const response = await fetch(`${foodApi}`);
   const data = await response.json();
-  const meal = data.categories.slice(1, 10);
+  const meal = data.categories;
   return meal;
 };
 
 const loadData = async () => {
+  const likesStorage = JSON.parse(localStorage.getItem('likes') || '[]');
+  console.log(likesStorage);
   const displayData = await getMeals();
   let display = '';
   displayData.forEach((element) => {
-    const check = element.completed ? 'checked' : '';
+    let likeVariable = 0;
+    likesStorage.forEach((likes) => {
+      if (element.idCategory === likes.item_id) {
+        likeVariable = likes.likes;
+      }
+    });
     display += `   
-    <li id="${element.idCategory}">
-    <img src="${element.strCategoryThumb}" alt="Food">
-    <h4>${element.strCategory}</h4>
-    <i class="far fa-heart like">${check}</i>
-    <button type="submit" class="pop" id="${element.idCategory}">Comments</button>
-    </li>`;
+      <li id="${element.idCategory}">
+      <img src="${element.strCategoryThumb}" alt="Food">
+      <h4>${element.strCategory}</h4>
+      <i data-id="${element.idCategory}" class="far fa-heart like"></i>
+      <span id="like${element.idCategory}" class="likes-count">${likeVariable} Likes</span>
+      <button type="submit" class="pop" id="${element.idCategory}">Comments</button>
+      </li>`;
   });
   mealList.innerHTML = display;
+  allHearts();
   accessComment();
+  await likesDisplay();
 };
+
+const totalItems = (arg) => {
+  let displayLength = '';
+  displayLength += `
+  <h3>Cuisine ${arg.length}</h3>`;
+  itemList.innerHTML = displayLength;
+};
+
+const fetchData = () => {
+  fetch('https://themealdb.com/api/json/v1/1/categories.php')
+    .then((response) => response.json())
+    .then((data) => totalItems(data.categories))
+    .catch((error) => new Error(error));
+};
+fetchData();
 
 export { loadData, getMeals };
---
+++ src/likes_api.js
@@ -0,0 +1,46 @@
+const involvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const apiId = 'PPe1jteASeyGFemQLBaU/likes';
+
+const likeId = async (id) => {
+  const response = await fetch(`${involvementApi}${apiId}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        // eslint-disable-next-line quote-props
+        'item_id': id,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+  const returnObject = await response.json;
+  return returnObject;
+};
+
+const getLikes = async () => {
+  const res = await fetch(`${involvementApi}${apiId}`);
+  return res.json();
+};
+
+const allHearts = async () => {
+  const allLikes = document.querySelectorAll('.like');
+  allLikes.forEach((like) => {
+    like.addEventListener('click', async (e) => {
+      const id = e.target.getAttribute('data-id');
+      await likeId(id);
+      const allLikesArray = await getLikes();
+      localStorage.setItem('likes', JSON.stringify(allLikesArray));
+      const getNewLikes = allLikesArray.find((element) => element.item_id === id).likes;
+      like.parentElement.querySelector(`#like${id}`).textContent = `${getNewLikes} Likes`;
+    });
+  });
+};
+
+const likesDisplay = async () => {
+  const allLikesArray = await getLikes();
+  allLikesArray.forEach((obj) => {
+    document.querySelector(`#like${obj.item_id}`).textContent = `${obj.likes} Likes`;
+  });
+};
+
+export { allHearts, likesDisplay };
