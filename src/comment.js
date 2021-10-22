05ff908e324f36f9d0e42947656c4fad5f15b327
Vagyasri <vagyasri2011@gmail.com>
2021-10-22 14:37:42 +0200

    Allign Images for popup

--- src/comment.js
+++ src/comment.js
@@ -9,18 +9,19 @@ const displayPop = async (index) => {
   <div class="popup-top">
   <button class="close"> Close </button>
     <div class="meal-data">
-    <h3> ${meals[index - 1].strCategory} <h3>
     <img src="${meals[index - 1].strCategoryThumb}" alt="s Image" class="meal-img">
+    <h3> ${meals[index - 1].strCategory} <h3>
     </div>
+
+    <p class="description"> ${meals[index - 1].strCategoryDescription} </p>
 
     <div class="com-count">
     <img src="https://c.tenor.com/CePpbsVdFRMAAAAC/love-beating.gif" alt="heart-icon" class="heart-icon"> <span class="like-count">likes count</span>
     </div>
-
-    <p class="description"> <b>Description:</b><br><br>${meals[index - 1].strCategoryDescription} </p>
 </div>
 
 <form class="popup-bottom">
+<h3> Add your comment</h3>
 <input type="text" class="userName" id="name-id" name="user-name" placeholder="Type Your Name" required>
 <textarea class="userComment" id="comment-id" name="comment-name" placeholder="Comment here ..." rows="10" cols="50" required></textarea>
 <button type="submit" class="comm-sub">Submit</button>
