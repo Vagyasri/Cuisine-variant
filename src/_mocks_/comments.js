b50d510123ae078ae79f4911f3ad0e1d5e9312e8
Vagyasri <vagyasri2011@gmail.com>
2021-10-23 18:09:59 +0200

    Add tests to the mock file

--- src/_mocks_/comments.js
+++ src/_mocks_/comments.js
@@ -1,6 +1,6 @@
 const commentsCounter = async () => {
   const result = Promise.resolve(
-    new Array(14).fill({
+    new Array(18).fill({
       comment: 'Indian Cuisine',
       creation_date: '2021-10-23',
       username: 'Lucky',
